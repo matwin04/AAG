843624ebbf86c374dabe1e7a5173f3ea704bd8a0
thememetsar <mattwiner04@gmail.com>
2024-05-09 15:19:17 -0700

    004

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,7 +6,15 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		// interface Platform {}
+		interface Platform {
+            env: {
+                DATABASE: sweettreats;
+            };
+            context: {
+                waitUntil(promise: Promise<any>): void;
+            };
+            caches: CacheStorage & { default: Cache };
+        }
 	}
 }
 
